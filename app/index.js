759d1cc9f559e03532ed23716222780763f5dc9b
HasanC14 <chowdhuryh999@gmail.com>
2023-10-15 23:50:40 +0600

    first commit

---
+++ app/index.js
@@ -0,0 +1,41 @@
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, FONT, SIZES, images } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+import { ScreenStackHeaderLeftView } from "react-native-screens";
+
+const Home = () => {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Nearbyjobs />
+          <Popularjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/hasan.png";
+
+export default {
+  profile,
+};
